fafc75a0a64fd4e25b1034ee44f33b348ff6b629
vanredian <red.yakubov@gmail.com>
2017-09-16 19:17:44 +0300

    first

---
+++ src/test/java/ua/redian/FirstTest.java
@@ -0,0 +1,57 @@
+package ua.redian;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    private WebDriver driver;
+
+    @BeforeClass // Runs this method before the first test method in the current class is invoked
+    public void setUp () {
+        // Create a new instance of the Firefox driver
+    }
+
+    @Test // Marking this method as part of the test
+    public void gotoSeleniumWikiPage () {
+        System.setProperty("webdriver.gecko.driver", "C:/server/geckodriver.exe");
+        driver = new FirefoxDriver();
+        // Go to the Wikipedia home page
+        driver.get("https://en.wikipedia.org/");
+        // Find the text input element by its id and type "Selenium"
+        driver.findElement(By.id("searchInput")).sendKeys("Selenium");
+        // Click search button
+        driver.findElement(By.id("searchButton")).click();
+        // Get text from header of the Selenium page
+        String header = driver.findElement(By.id("firstHeading")).getText();
+        // Verify that header equals "Selenium"
+        Assert.assertEquals(header, "Selenium");
+    }
+
+    @Test // Marking this method as part of the test
+    public void gotoSeleniumWikiPageFailure () {
+        System.setProperty("webdriver.gecko.driver", "C:\\server\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        // Go to the Wikipedia home page
+        driver.get("https://en.wikipedia.org/");
+        // Find the text input element by its id and type "Selenium"
+        driver.findElement(By.id("searchInput")).sendKeys("Selenium");
+        // Click search button
+        driver.findElement(By.id("searchButton")).click();
+        // Get text from header of the Selenium page
+        String header = driver.findElement(By.id("firstHeading")).getText();
+        // Verify that header equals "Selenium WebDriver"
+        Assert.assertEquals(header, "Selenium WebDriver");
+    }
+
+    @AfterClass // Runs this method after all the test methods in the current class have been run
+    public void tearDown () {
+        // Close all browser windows and safely end the session
+        driver.quit();
+    }
+}
